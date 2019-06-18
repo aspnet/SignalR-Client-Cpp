f8ea96aeed47a99dcdb92e7a45078f3d482ec8cb
Brennan Conroy <brecon@microsoft.com>
2019-06-18 15:57:17 -0700

    crazy timeout

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
 echo "Running executable"
-eval $*
-EXITCODE=$?
-echo "Finished running with exit code: " $EXITCODE
+
+PID=0
+EXITCODE=1
+
+# launch process in background
+eval $* &
+# capture process ID to monitor
+PID=$!
+
+# Timeout duration
+TIME=120
+
+while (($TIME > 0)); do
+  # polling interval
+  sleep 5
+
+  # check if process is still running
+  ps ax | grep $PID | grep -v grep > /dev/null
+  if test "$?" != "0"
+  then
+    # process exited so lets get the exit code
+    wait $PID
+    EXITCODE=$?
+    echo "Finished running with exit code: "$EXITCODE
+    exit $EXITCODE
+  fi
+  ((TIME -= 5))
+done
+
+# process taking too long, kill it
+kill -9 $PID
+
+echo "Process took too long"
 exit $EXITCODE
