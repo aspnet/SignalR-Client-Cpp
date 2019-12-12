6b9bce68791c1fc6be995f133712daee58e02943
Bill Romanowich <bill.romanowich@sealedair.com>
2019-12-12 09:29:19 -0500

    Simplify logic when dealing with integer values to only use int64_t for negative numbers > INT64_MIN, uint64_t for 0 and positive numbers < UINT64_MAX and to continue as double (float64_t) otherwise.

--- src/signalrclient/json_helpers.cpp
+++ src/signalrclient/json_helpers.cpp
@@ -60,34 +60,28 @@ namespace signalr
             {
 				if (value < 0)
 				{
-					if (value > (double)INT32_MIN)
+					if (value > (double)INT64_MIN)
 					{
-						// Fits within int32_t
-						return Json::Value((int32_t)intPart);
+						// Fits within int64_t
+						return Json::Value((int64_t)intPart);
 					}
 					else
 					{
-						// Needs int64_t
-						return Json::Value((int64_t)intPart);
+						// Remain as double
+						return Json::Value(value);
 					}
 				}
 				else
 				{
-					if (value < INT32_MAX)
+					if (value < UINT64_MAX)
 					{
-						return Json::Value((int32_t)intPart);
-					}
-					else if (value < UINT32_MAX)
-					{
-						return Json::Value((uint32_t)intPart);
-					}
-					else if (value < INT64_MAX)
-					{
-						return Json::Value((int64_t)intPart);
+						// Fits within uint64_t
+						return Json::Value((uint64_t)intPart);
 					}
 					else
 					{
-						return Json::Value((uint64_t)intPart);
+						// Remain as double
+						return Json::Value(value);
 					}
 				}
             }
