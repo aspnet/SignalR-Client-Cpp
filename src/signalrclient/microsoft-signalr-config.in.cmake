350658c67a075e0c385fe884ffe6079aa73438cd
BrennanConroy <brecon@microsoft.com>
2021-03-22 14:23:37 -0700

    fixups

--- src/signalrclient/microsoft-signalr-config.in.cmake
+++ src/signalrclient/microsoft-signalr-config.in.cmake
@@ -1,5 +1,11 @@
+include(CMakeFindDependencyMacro)
+
 if(@USE_CPPRESTSDK@)
   find_dependency(cpprestsdk)
+endif()
+
+if(@INCLUDE_JSONCPP@)
+  find_dependency(${JSONCPP_LIB})
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/microsoft-signalr-targets.cmake")
